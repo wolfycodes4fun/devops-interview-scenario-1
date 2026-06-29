d50c724f012d0bafec2830f0991834e14308aab6
Ametha Jayawardena 🐺 <amethacodes@gmail.com>
2026-06-30 01:19:20 +0530

    merge branch 'ametha/task_01_setup_minikube_cluster' to 'main'

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    minikube = {
+      source  = "scott-the-programmer/minikube"
+      version = "0.6.0"
+    }
+  }
+}
+
+provider "minikube" {
+  kubernetes_version = "v1.30.2"
+}
+
+resource "minikube_cluster" "local" {
+  driver       = "docker"
+  cluster_name = "task-01-cluster"
+  addons = [
+    "default-storageclass",
+    "storage-provisioner"
+  ]
+  nodes = 2
+  cni = "calico"
+}
